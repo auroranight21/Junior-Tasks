e2e2606f33a7c5950b4c97439b67d2131c0abb31
Aurora Night <auroranight@MacBook-Pro-Aurora.local>
2022-10-19 20:12:57 +0300

    push elements in the array

---
+++ Task 19/index.js
@@ -0,0 +1,13 @@
+/** ЗАДАЧА 19 - Добавление элементов в массив
+ *
+ * 1. Создайте любой массив
+ *
+ * 2. Добавьте в конец массива несколько новых элементов
+ *
+ * 3. Выведите в консоль длину результирующего массива
+ */
+
+
+const carsArray = ['Tesla', 'Ferrari', 'Lamborghini'];
+carsArray.push('Bugatti', 'Audi', 'Mercedes');
+console.log(carsArray.length);
