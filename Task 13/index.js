6cf38dfa9de85ebf5dc33b08f613aee2de468557
Aurora Night <auroranight@MacBook-Pro-Aurora.local>
2022-10-19 16:30:17 +0300

    string length task

---
+++ Task 13/index.js
@@ -0,0 +1,8 @@
+/** ЗАДАЧА 13 - Длина строки
+ *
+ * 1. Создайте переменную и присвойте ей любую строку
+ *
+ * 2. Виведите в консоль длину этой строки
+ */
+const phraseLength = 'My favorite car is Tesla Model X';
+console.log(`Phrase length is: ${phraseLength.length}`);
